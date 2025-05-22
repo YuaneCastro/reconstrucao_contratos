b2100cfcea8f267ca3fdb68a9826a1f0a3c892c1
YuaneCastro <yuanecastroff21@gmail.com>
2025-05-22 19:34:39 +0100

    sss

---
+++ .history/src/db/connection_20250521203448.js
@@ -0,0 +1,25 @@
+/*require('dotenv').config();
+const { Pool } = require('pg');
+
+// Utilizando DATABASE_URL, que é configurada automaticamente pelo Railway
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+module.exports = pool;*/
+
+/*require('dotenv').config();
+require('dotenv').config();
+const { Pool } = require('pg');
+
+// Utilizando DATABASE_URL, que é configurada automaticamente pelo Railway
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
+module.exports = pool;8
---
+++ .history/src/routers/dashboardRoutes_20250521225159.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const telapCont = require('../controllers/dashboard');
+const authenticateToken = require('../middlewares/auth');
+const authorizeRole = require('../middlewares/authorizerole');
+
+router.get('/Telaerro', telapCont.telaerror);
+
+
+// -----------coordenacao--------------
+router.get('/dashboard-cordenacao', authenticateToken, authorizeRole('administracao', 'secretaria'),telapCont.dashboard_cordenacao);
+
+router.post('/update_student', authenticateToken, authorizeRole('administracao', 'secretaria'), telapCont.update_Student);
+router.post('/update_encarregado', authenticateToken, authorizeRole('administracao', 'secretaria'), telapCont.update_encarregado);
+router.post('/deletar_encarregados', authenticateToken, authorizeRole('administracao', 'secretaria'), telapCont.deletar);
+router.post('/redifinir_senha',authenticateToken, authorizeRole('coordenador'), telapCont.redifinir_senha);
+router.post('/enviar_documento',authenticateToken, authorizeRole('coordenador'), telapCont.enviar_documento);   
+router.post('/guardar_documento',authenticateToken, authorizeRole('coordenador'), telapCont.guardar_documentos);
+router.post('/associados',authenticateToken, authorizeRole('coordenador'), telapCont.encarregados_associados);
+
+//-----------encarregados---------------
+router.get('/dashboard', authenticateToken, authorizeRole('encarregado'), telapCont.showtelap);
+router.get('/documentos/:id',authenticateToken, authorizeRole('encarregado'), telapCont.buscar_documento);
+router.post('/assinar',authenticateToken, authorizeRole('encarregado'), telapCont.assinar_contrato);
+
+module.exports = router;
